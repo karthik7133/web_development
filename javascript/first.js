2621d2bdd7ed6c487db869a88848e9d219d71e3b
karthik <chkarthik853@gmail.com>
2025-05-26 09:26:31 +0530

    screenshorts commit

--- javascript/first.js
+++ javascript/first.js
@@ -30,3 +30,34 @@ const pen={
     discount:5
 }
 console.log(pen)
+num=3;
+num**=2;
+console.log(num);
+
+// let no=prompt("Enter the number:");
+// if(no%5==0)
+//     console.log("Its devisible by 5")
+// else console.log("its not divisible")
+
+// let marks=prompt("Enter marks");
+// if(marks>=80 & marks<=100){
+//     console.log("A")
+// }else if(70>=marks>80){
+//     console.log('B')
+// }else if(60>=marks>70){
+//     console.log('C')
+// }else if(50>=marks>60){
+//     console.log('D')
+// }else{
+//     console.log('F')
+// }
+
+for(let c of "karthik"){
+    console.log(c);
+}
+for(let d in "karhtik"){
+    console.log(d)
+}
+for(let k in pen){
+    console.log(pen[k])
+}
